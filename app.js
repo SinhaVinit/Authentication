95bcb75be05acae99fdcfb942ea442c1c62d4db9
Vinit Sinha <vinitsinha@Vinits-MacBook-Air.local>
2022-02-03 21:59:11 +0530

    fourth commit

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -34,23 +35,25 @@ app.get("/login", function(req,res) {
 });
 
 app.post("/register", function(req,res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save(function(err) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.render("secrets");
+            }
+        });
     });
 });
 
 app.post("/login", function(req,res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, function(err, foundUser) {
         if (err) {
@@ -58,12 +61,15 @@ app.post("/login", function(req,res) {
         }
         else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
-                else {
-                    res.send("Wrong Password");
-                }
+                // Load hash from your password DB.
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+                    }
+                    else {
+                        res.send("Wrong Password");
+                    }
+                });
             }
         }
     });
